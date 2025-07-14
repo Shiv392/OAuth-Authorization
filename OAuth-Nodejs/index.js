0fc1f63902cd109f70000d3e901396ecbe0830d4
Shiv Soni <shiv@leadangel.com>
2025-07-14 23:13:04 +0530

    added db connection file

--- OAuth-Nodejs/index.js
+++ OAuth-Nodejs/index.js
@@ -4,12 +4,21 @@ const body_parser = require('body-parser');
 const cors = require('cors');
 const port = 8800;
 
+const {mysql_connection} = require('./db/db_connection.js');
+
 
 app.use(body_parser.json());
 app.use(cors());
 
 app.get('/',(req,res)=>{
     return res.send(`<h1>This is Home Route`);
+})
+
+mysql_connection.connect((err)=>{
+    if(err){
+        console.log('database connection failed',err);
+    }
+    console.log('database connection sucessfull')
 })
 
 app.listen(port,()=>{
