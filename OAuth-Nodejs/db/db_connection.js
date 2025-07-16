45abd5b2a75fa67c4f0ecacf940d3b58d516be3f
Shiv Soni <sonishiv309@gmail.com>
2025-07-17 00:45:31 +0530

    installed node js security and created client id and access token in Google Oauth service.

--- OAuth-Nodejs/db/db_connection.js
+++ OAuth-Nodejs/db/db_connection.js
@@ -3,10 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mysql_connection = mysql.createConnection({
-    host : process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password : process.env.DATABASE_PASSWORD,
-    user: process.env.DATABASE_USER
+    // host : process.env.DATABASE_HOST,
+    host : 'localhost',
+    // database: process.env.DATABASE_NAME,
+    database : 'Oauth_implement',
+    // password : process.env.DATABASE_PASSWORD,
+    password : 'Shiv@3923',
+    // user: process.env.DATABASE_USER
+    user : 'root'
 })
 
 module.exports={mysql_connection}
