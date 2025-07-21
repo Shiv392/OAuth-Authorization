7dda68cd691f8c6a0cfb174a7ab3831135149a72
Shiv Soni <sonishiv309@gmail.com>
2025-07-21 23:51:29 +0530

    created layout compoennt

---
+++ Oauth-project/src/features/layout/pages/layout.tsx
@@ -0,0 +1,8 @@
+import LayoutComponent from "../components/layout-component";
+
+const LayoutPage = ()=>{
+return(
+   <LayoutComponent />
+)
+}
+export default LayoutPage;
--- Oauth-project/src/routes/index.tsx
+++ Oauth-project/src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { Suspense } from "react";
 import { AuthRoutes } from "../features/authentication/routes";
 import { useRoutes } from "react-router-dom";
+import LayoutRoutes from "../features/layout/routes";
 
-const routes = [...AuthRoutes];
+const routes = [...AuthRoutes, ...LayoutRoutes];
 
 const AppRoutes = ()=>{
 const elements = useRoutes(routes);
