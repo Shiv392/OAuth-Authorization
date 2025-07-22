d3ea40a8d6c469297a12740b7246688deee9b3f2
Shiv Soni <sonishiv309@gmail.com>
2025-07-23 00:23:44 +0530

    created seprate module for each controller and routes type.

--- OAuth-Nodejs/routes/oauthorization/google_auth_redirect.js
+++ OAuth-Nodejs/routes/oauthorization/google_auth_redirect.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const google_auth_routes = express.Router();
-const {google_auth_redirect} = require('../controllers/google_auth_redirect.js')
+const {google_auth_redirect} = require('../../controllers/oauthorization/google_auth_redirect')
 
 google_auth_routes.get('/auth/google',google_auth_redirect);
 
--- OAuth-Nodejs/routes/oauthorization/google_callback_redirect.js
+++ OAuth-Nodejs/routes/oauthorization/google_callback_redirect.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const google_callback_route = express.Router();
 
-const {google_callback_redirect} = require('../controllers/google_callback.js');
+const {google_callback_redirect} = require('../../controllers/oauthorization/google_callback');
 
 google_callback_route.get('/auth/google/callback',google_callback_redirect);
 
