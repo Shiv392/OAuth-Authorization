73c9c50a02c22ba4d05c4b8bbb816495f2ea4c60
Shiv Soni <sonishiv309@gmail.com>
2025-07-24 22:50:09 +0530

    completed oauthorization function

--- OAuth-Nodejs/models/oauthorization/oauthorization.js
+++ OAuth-Nodejs/models/oauthorization/oauthorization.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const OAuthorization = ({ id, name, email, picture }) => {
     console.log('id--->', id, 'name--->', name, 'email--->', email, 'picture--->', picture);
 
-    let find_user  = {};
+    let find_user = {};
 
     return new Promise((resolve, reject) => {
         const select_user_query = `select * from user where email = ?`;
@@ -16,26 +16,21 @@ const OAuthorization = ({ id, name, email, picture }) => {
 
             if (user.length == 0) { //no user found with email, then create a new one --->
                 const add_user_query = `insert into user(name,email,profile) values(?,?,?)`;
-                mysql_connection.query(add_user_query, [name, email, picture], (add_err) => {
+                mysql_connection.query(add_user_query, [name, email, picture], (add_err, new_user) => {
                     if (add_err) {
                         console.log('add user error---->', add_err);
                         return reject(add_err);
                     }
 
-                const user_search_query = `select id from user where email = ?`;
-                mysql_connection.query(user_search_query,[email],(search_err,user_id)=>{
-                    if(search_err){
-                        return reject(search_err);
-                    }
-                    const jwt_token = jwt.sign({ user_id: user_id, email: email }, 'Shiv39231100', { expiresIn: '1hr' });
+                    const newuser_id = new_user.insertId;
+                    const jwt_token = jwt.sign({ user_id: newuser_id, email: email }, process.env.JWT_SECRET, { expiresIn: '1hr' });
                     return resolve({ success: true, message: 'Login Successfull', token: jwt_token });
-                })
                 })
             }
             else { //if user already exists --->
                 find_user = user[0];
-                console.log('stored user---->',find_user);
-                const jwt_token = jwt.sign({ user_id: find_user.id, email : find_user.email  }, 'Shiv39231100', {
+                console.log('stored user---->', find_user);
+                const jwt_token = jwt.sign({ user_id: find_user.id, email: find_user.email }, process.env.JWT_SECRET, {
                     expiresIn: '1h',
                 });
                 return resolve({
