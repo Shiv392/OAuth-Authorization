dea50735c5a1e727c49bbc2c0202ee7cf622282b
Shiv Soni <sonishiv309@gmail.com>
2025-08-02 15:34:47 +0530

    create navbar and usser info componet to show user information

--- Oauth-project/src/features/layout/components/layout-component.tsx
+++ Oauth-project/src/features/layout/components/layout-component.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import useUserProfile from "../hooks/useUserprofile";
+import Navbar from "./navbar";
+import User_info from "./user-info";
 
 const LayoutComponent = ()=>{
 const [user_data, set_user_data] = useState<any>({});
 const {get_user_profile, error} = useUserProfile();
+const [login_user, set_login_user] = useState(false);
 
 useEffect(()=>{
 console.log(get_user_profile, error);
@@ -17,7 +20,15 @@ set_user_data(data);
 }
 
 return(
-    <h1>Layout Component</h1>
+   <div>
+    <nav>
+    <Navbar />
+    </nav>
+
+    <section>
+        <User_info />
+    </section>
+   </div>
 )
 }
 export default LayoutComponent;
---
+++ Oauth-project/src/features/layout/components/user-info.tsx
@@ -0,0 +1,6 @@
+const User_info = ()=>{
+return(
+    <h1>User Info Section</h1>
+)
+}
+export default User_info;
