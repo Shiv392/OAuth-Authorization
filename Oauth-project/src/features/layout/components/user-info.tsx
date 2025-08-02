96f6c8b5e41934c1b2857c5f681b71514e80107d
Shiv Soni <sonishiv309@gmail.com>
2025-08-02 16:00:54 +0530

    created user info compoentn and showin dat as list

--- Oauth-project/src/features/layout/components/user-info.tsx
+++ Oauth-project/src/features/layout/components/user-info.tsx
@@ -1,6 +1,39 @@
+
+type user_model={
+label : string,
+value : string
+}
+
+type user_details_model = Array<user_model>;
+const user_info : user_details_model = [
+    {
+        label : 'Name',
+        value : 'Shiv Soni'
+    },
+    {
+        label : 'Email',
+        value : 'sonishiv309@gmail.com'
+    }
+]
 const User_info = ()=>{
 return(
-    <h1>User Info Section</h1>
+    <div className="mt-16 flex flex-col justify-center items-center">
+        <h3>User Details</h3>
+       <div className="w-[370px] ">
+        <ul>
+        {
+            user_info.map((user,index)=>(
+                <li key={index} className="mb-3 border-b-2 p-3">
+                    <div className="flex justify-between ">
+                      <span>{user.label}</span>
+                      <span>{user.value}</span>
+                    </div>
+                </li>
+            ))
+        }
+        </ul>
+       </div>
+    </div>
 )
 }
 export default User_info;
