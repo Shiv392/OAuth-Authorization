6de1796e5c7cf85ea3f357758790cb1e95f17c11
Shiv Soni <sonishiv309@gmail.com>
2025-08-03 23:49:06 +0530

    completed authentication and authorization middlware

--- Oauth-project/src/features/layout/components/layout-component.tsx
+++ Oauth-project/src/features/layout/components/layout-component.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { lazy, useEffect, useState } from "react";
 import useUserProfile from "../hooks/useUserprofile";
-import Navbar from "./navbar";
-import User_info from "./user-info";
+
+const Navbar = lazy(()=> import('./navbar'));
+const User_info = lazy(()=> import('./user-info'));
 
 const LayoutComponent = ()=>{
 const [user_data, set_user_data] = useState<any>({});
@@ -9,7 +10,6 @@ const {get_user_profile, error} = useUserProfile();
 const [login_user, set_login_user] = useState(false);
 
 useEffect(()=>{
-console.log(get_user_profile, error);
 call_user_detail();
 },[]);
 
